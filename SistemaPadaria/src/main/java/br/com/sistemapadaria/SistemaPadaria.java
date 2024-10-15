fadf8c54ff6e212582e1a2ac960ee3372ed2763b
Ana Luiza Marcal Freitas <1030365@senacminas.edu.br>
2024-10-15 19:50:59 -0300

    Iniciando projeto.

---
+++ SistemaPadaria/src/main/java/br/com/sistemapadaria/SistemaPadaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.sistemapadaria;
+
+/**
+ *
+ * @author 1030365
+ */
+public class SistemaPadaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
